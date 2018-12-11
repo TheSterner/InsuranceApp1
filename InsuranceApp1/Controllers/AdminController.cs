ad91cff074915a31cb61fe2bf23c15b5554c6e08
William B McAllister <blakemcallister77@gmail.com>
2018-12-11 13:26:57 -0500

    InsuranceApp1

---
+++ InsuranceApp1/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using InsuranceApp1.Models;
+using InsuranceApp1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsuranceApp1.Controllers
+{
+    public class AdminController : Controller
+    {
+		// GET: Admin
+		public ActionResult Index()
+		{
+			using (Insurance2Entities db = new Insurance2Entities())
+			{
+				var customerInfos = db.CustomerInfoes.ToList();
+				var customerInfoVms = new List<CustomerInfoVm>();
+				foreach (var customerInfo in customerInfos)
+				{
+					var customerInfoVm = new CustomerInfoVm();
+					customerInfoVm.Id = customerInfo.Id;
+					customerInfoVm.FirstName = customerInfo.FirstName;
+					customerInfoVm.LastName = customerInfo.LastName;
+					customerInfoVm.EmailAddress = customerInfo.EmailAddress;
+					customerInfoVm.Quote = customerInfo.Quote.ToString();
+					customerInfoVms.Add(customerInfoVm);
+				}
+			return View(customerInfoVms);
+			}
+        }
+    }
+}
--- InsuranceApp1/Controllers/HomeController.cs
+++ InsuranceApp1/Controllers/HomeController.cs
@@ -1,30 +1,124 @@
-﻿using System;
+﻿using InsuranceApp1.Models;
+using InsuranceApp1.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace InsuranceApp1.Controllers
 {
 	public class HomeController : Controller
 	{
+
 		public ActionResult Index()
 		{
 			return View();
 		}
 
-		public ActionResult About()
+		//private CustomerInfo db = new CustomerInfo();
+		private Insurance2Entities db = new Insurance2Entities();
+
+
+		[HttpPost]
+		public ActionResult CustomerInfo(string firstName, string lastName, string emailAddress, string CustomerDOB, int carYear,
+										   string carMake, string carModel, bool dui, string speedingTicket, bool fullCoverage)
+
 		{
-			ViewBag.Message = "Your application description page.";
+			//Check for Blanks
+			if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) || string.IsNullOrEmpty(CustomerDOB.ToString()) ||
+				string.IsNullOrEmpty(carYear.ToString()) || string.IsNullOrEmpty(carMake) || string.IsNullOrEmpty(carModel) || string.IsNullOrEmpty(speedingTicket.ToString()))
+			{
+				return View("~/Views/Shared/Error.cshtml");
 
-			return View();
-		}
+			}
+			else
+			{
+				using (Insurance2Entities db = new Insurance2Entities())
+				{
+					var customerInfo = new CustomerInfo();
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+					customerInfo.FirstName = firstName;
+					customerInfo.LastName = lastName;
+					customerInfo.EmailAddress = emailAddress;
+					customerInfo.CustomerDOB = DateTime.Parse(CustomerDOB);
+					customerInfo.CarYear = carYear;
+					customerInfo.CarMake = carMake;
+					carMake = carMake.ToLower();
+					customerInfo.CarModel = carModel;
+					carModel = carModel.ToLower();
+					customerInfo.DUI = dui;
+					customerInfo.SpeedingTickets = Convert.ToInt32(speedingTicket);
+					customerInfo.FullCoverage = fullCoverage;
 
-			return View();
+
+					DateTime grab = DateTime.Parse(CustomerDOB);
+					int year = grab.Year;
+					int age = DateTime.Now.Year - year;
+
+					double quote;
+					quote = 50.00;
+
+					//Age
+					if (age < 18)
+					{
+						quote = quote + 100.00;
+					}
+					else if (age < 25 || age > 100)
+					{
+						quote = quote + 25.00;
+					}
+
+					//Car Yr
+					if (carYear == 1999 || carYear > 2015)
+					{
+						quote = quote + 25.00;
+					}
+
+					//Check Car Model for Porsche 
+					if (carMake == "porsche")
+					{
+						quote = quote + 25.00;
+						if (carModel == "911 carrera")
+						{
+							quote = quote + 25.00;
+						}
+					}
+
+					//Check for Speeding Tickets	
+					int ticketCount = Convert.ToInt32(speedingTicket);
+					if (ticketCount < 0)
+					{
+						return View("~/Views/Shared/Error.cshtml");
+					}
+					for (int i = 0; i < ticketCount; i++)
+					{
+						quote = quote + 10.00;
+					}
+
+					//DUI
+					if (dui == true)
+					{
+						quote = quote + (quote * .25);
+					}
+
+					//Full Coverage Check
+					if (fullCoverage == true)
+					{
+						quote = quote + (quote * .50);
+					}
+
+					customerInfo.Quote = quote;
+					int userId = customerInfo.Id;
+					db.CustomerInfoes.Add(customerInfo);
+					db.SaveChanges();
+
+					//int userId = customerInfo.Id;
+
+					return RedirectToAction("Quote", new { Id = userId });
+				}
+			}
 		}
 	}
 }
